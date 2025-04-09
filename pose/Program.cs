1cb0e21be9683781ae025b2b3f7022bf16c690b8
C.A.Tucker <cartheur@pm.me>
2025-04-09 19:15:27 +0200

    Comment previous coding attempt

--- pose/Program.cs
+++ pose/Program.cs
@@ -7,23 +7,25 @@ using ConsoleTables;
 class PoseReader
 {
     public static MotorFunctions MotorControl { get; set; }
-    public static MotorSequence MotorSequenceAbdomen { get; set; }
-    public static MotorSequence MotorSequenceBust { get; set; }
-    public static MotorSequence MotorSequenceLeftArm { get; set; }
-    public static MotorSequence MotorSequenceLeftLeg { get; set; }
-    public static MotorSequence MotorSequenceRightArm { get; set; }
-    public static MotorSequence MotorSequenceRightLeg { get; set; }
+    public static MotorSequence MotorSequenceAll { get; set; }
+    //public static MotorSequence MotorSequenceAbdomen { get; set; }
+    //public static MotorSequence MotorSequenceBust { get; set; }
+    //public static MotorSequence MotorSequenceLeftArm { get; set; }
+    //public static MotorSequence MotorSequenceLeftLeg { get; set; }
+    //public static MotorSequence MotorSequenceRightArm { get; set; }
+    //public static MotorSequence MotorSequenceRightLeg { get; set; }
     public static bool MotorsInitialized { get; set; }
 
     static async Task Main()
     {
         MotorControl = new MotorFunctions();
-        MotorSequenceAbdomen = new MotorSequence();
-        MotorSequenceBust = new MotorSequence();
-        MotorSequenceLeftArm = new MotorSequence();
-        MotorSequenceLeftLeg = new MotorSequence();
-        MotorSequenceRightArm = new MotorSequence();
-        MotorSequenceRightLeg = new MotorSequence();
+        MotorSequenceAll = new MotorSequence();
+        //MotorSequenceAbdomen = new MotorSequence();
+        //MotorSequenceBust = new MotorSequence();
+        //MotorSequenceLeftArm = new MotorSequence();
+        //MotorSequenceLeftLeg = new MotorSequence();
+        //MotorSequenceRightArm = new MotorSequence();
+        //MotorSequenceRightLeg = new MotorSequence();
         Console.WriteLine(MotorFunctions.InitializeDynamixelMotors());
         MotorsInitialized = MotorFunctions.DynamixelMotorsInitialized;
         MotorFunctions.CollateMotorArray();
@@ -37,68 +39,77 @@ class PoseReader
         // Read and display current positions.
         Console.WriteLine("\nSitting pose:" + Environment.NewLine);
         Logging.WriteLog("Sitting pose:" + Environment.NewLine, Logging.LogType.Data, Logging.LogCaller.JoiPose);
-    // Bust, Head, LeftArm, RightArm, LeftPelvis, RightPelvis, 
     repeat:
-        // Abdomen.
-        var abdomen = MotorSequenceAbdomen.ReturnDictionaryOfPositions(Limbic.Abdomen);
-        var tableAbdomen = new ConsoleTable("abdomen", "position");
-        foreach (var kvpa in abdomen)
+        // All.
+        var all = MotorSequenceAll.ReturnDictionaryOfPositions(Limbic.All);
+        var tableAll = new ConsoleTable("motor", "position");
+        foreach (var kvp in all)
         {
-            tableAbdomen.AddRow(kvpa.Key, kvpa.Value);
+            tableAll.AddRow(kvp.Key, kvp.Value);
         }
-        tableAbdomen.Write();
+        tableAll.Write();
         Console.WriteLine();
-        Logging.WriteLog(tableAbdomen.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
-        // Bust.
-        var bust = MotorSequenceBust.ReturnDictionaryOfPositions(Limbic.Bust);
-        var tableBust = new ConsoleTable("bust", "position");
-        foreach (var kvpb in bust)
-        {
-            tableBust.AddRow(kvpb.Key, kvpb.Value);
-        }
-        tableBust.Write();
-        Console.WriteLine();
-        Logging.WriteLog(tableBust.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
-        // Left arm.
-        var leftArm = MotorSequenceLeftArm.ReturnDictionaryOfPositions(Limbic.LeftArm);
-        var tableLeftArm = new ConsoleTable("leftarm", "position");
-        foreach (var kvpla in leftArm)
-        {
-            tableLeftArm.AddRow(kvpla.Key, kvpla.Value);
-        }
-        tableLeftArm.Write();
-        Console.WriteLine();
-        Logging.WriteLog(tableLeftArm.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
-        // Left leg.
-        var leftLeg = MotorSequenceLeftLeg.ReturnDictionaryOfPositions(Limbic.LeftLeg);
-        var tableLeftLeg = new ConsoleTable("leftleg", "position");
-        foreach (var kvpl in leftLeg)
-        {
-            tableLeftLeg.AddRow(kvpl.Key, kvpl.Value);
-        }
-        tableLeftLeg.Write();
-        Console.WriteLine();
-        Logging.WriteLog(tableLeftLeg.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
-        // Right arm.
-        var rightArm = MotorSequenceRightArm.ReturnDictionaryOfPositions(Limbic.RightArm);
-        var tablerightArm = new ConsoleTable("rightarm", "position");
-        foreach (var kvpr in rightArm)
-        {
-            tablerightArm.AddRow(kvpr.Key, kvpr.Value);
-        }
-        tablerightArm.Write();
-        Console.WriteLine();
-        Logging.WriteLog(tablerightArm.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
-        // Right leg.
-        var rightLeg = MotorSequenceRightLeg.ReturnDictionaryOfPositions(Limbic.RightLeg);
-        var tablerightLeg = new ConsoleTable("rightleg", "position");
-        foreach (var kvpr in rightLeg)
-        {
-            tablerightLeg.AddRow(kvpr.Key, kvpr.Value);
-        }
-        tablerightLeg.Write();
-        Console.WriteLine();
-        Logging.WriteLog(tablerightLeg.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
+        Logging.WriteLog(tableAll.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose, "all-demm");
+        //// Abdomen.
+        //var abdomen = MotorSequenceAbdomen.ReturnDictionaryOfPositions(Limbic.Abdomen);
+        //var tableAbdomen = new ConsoleTable("abdomen", "position");
+        //foreach (var kvpa in abdomen)
+        //{
+        //    tableAbdomen.AddRow(kvpa.Key, kvpa.Value);
+        //}
+        //tableAbdomen.Write();
+        //Console.WriteLine();
+        //Logging.WriteLog(tableAbdomen.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
+        //// Bust.
+        //var bust = MotorSequenceBust.ReturnDictionaryOfPositions(Limbic.Bust);
+        //var tableBust = new ConsoleTable("bust", "position");
+        //foreach (var kvpb in bust)
+        //{
+        //    tableBust.AddRow(kvpb.Key, kvpb.Value);
+        //}
+        //tableBust.Write();
+        //Console.WriteLine();
+        //Logging.WriteLog(tableBust.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
+        //// Left arm.
+        //var leftArm = MotorSequenceLeftArm.ReturnDictionaryOfPositions(Limbic.LeftArm);
+        //var tableLeftArm = new ConsoleTable("leftarm", "position");
+        //foreach (var kvpla in leftArm)
+        //{
+        //    tableLeftArm.AddRow(kvpla.Key, kvpla.Value);
+        //}
+        //tableLeftArm.Write();
+        //Console.WriteLine();
+        //Logging.WriteLog(tableLeftArm.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
+        //// Left leg.
+        //var leftLeg = MotorSequenceLeftLeg.ReturnDictionaryOfPositions(Limbic.LeftLeg);
+        //var tableLeftLeg = new ConsoleTable("leftleg", "position");
+        //foreach (var kvpl in leftLeg)
+        //{
+        //    tableLeftLeg.AddRow(kvpl.Key, kvpl.Value);
+        //}
+        //tableLeftLeg.Write();
+        //Console.WriteLine();
+        //Logging.WriteLog(tableLeftLeg.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
+        //// Right arm.
+        //var rightArm = MotorSequenceRightArm.ReturnDictionaryOfPositions(Limbic.RightArm);
+        //var tablerightArm = new ConsoleTable("rightarm", "position");
+        //foreach (var kvpr in rightArm)
+        //{
+        //    tablerightArm.AddRow(kvpr.Key, kvpr.Value);
+        //}
+        //tablerightArm.Write();
+        //Console.WriteLine();
+        //Logging.WriteLog(tablerightArm.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
+        //// Right leg.
+        //var rightLeg = MotorSequenceRightLeg.ReturnDictionaryOfPositions(Limbic.RightLeg);
+        //var tablerightLeg = new ConsoleTable("rightleg", "position");
+        //foreach (var kvpr in rightLeg)
+        //{
+        //    tablerightLeg.AddRow(kvpr.Key, kvpr.Value);
+        //}
+        //tablerightLeg.Write();
+        //Console.WriteLine();
+        //Logging.WriteLog(tablerightLeg.ToString(), Logging.LogType.Data, Logging.LogCaller.JoiPose);
 
         Console.WriteLine("If wanting to rescan, type 'do', otherwise Enter to exit.");
         var input = Console.ReadLine(); // Wait for you to view the position values.
