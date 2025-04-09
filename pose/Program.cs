4e65e03dbc9b776447a47097b4592d69819ae769
C.A.Tucker <cartheur@pm.me>
2025-04-09 12:44:44 +0200

    Collect functions and add rescan

--- pose/Program.cs
+++ pose/Program.cs
@@ -4,13 +4,17 @@ using ConsoleTables;
 class PoseReader
 {
     public static MotorFunctions MotorControl { get; set; }
-    public static MotorSequence MotorSequences { get; set; }
+    public static MotorSequence MotorSequenceAbdomen { get; set; }
+    public static MotorSequence MotorSequenceLeftLeg { get; set; }
+    public static MotorSequence MotorSequenceRightLeg { get; set; }
     public static bool MotorsInitialized { get; set; }
 
     static async Task Main()
     {
         MotorControl = new MotorFunctions();
-        MotorSequences = new MotorSequence();
+        MotorSequenceAbdomen = new MotorSequence();
+        MotorSequenceLeftLeg = new MotorSequence();
+        MotorSequenceRightLeg = new MotorSequence();
         Console.WriteLine(MotorFunctions.InitializeDynamixelMotors());
         MotorsInitialized = MotorFunctions.DynamixelMotorsInitialized;
         MotorFunctions.CollateMotorArray();
@@ -22,36 +26,38 @@ class PoseReader
         Console.ReadLine(); // Wait for you to position it.
 
         // Read and display current positions.
-        var abdomen = MotorSequences.ReturnDictionaryOfPositions(Limbic.Abdomen);
-        var leftLeg = MotorSequences.ReturnDictionaryOfPositions(Limbic.LeftLeg);
-        var rightLeg = MotorSequences.ReturnDictionaryOfPositions(Limbic.RightLeg);
-
-        // Output as code-friendly format.
         Console.WriteLine("\nStanding pose dictionary:" + Environment.NewLine);
-
+    repeat:
+        var abdomen = MotorSequenceAbdomen.ReturnDictionaryOfPositions(Limbic.Abdomen);
         var tableAbdomen = new ConsoleTable("abdomen-motor", "position");
-        var tableLeftLeg = new ConsoleTable("left-leg-motor", "position");
-        var tablerightLeg = new ConsoleTable("right-leg-motor", "position");
-
-        foreach (var kvp in abdomen)
+        foreach (var kvpa in abdomen)
         {
-            tableAbdomen.AddRow(kvp.Key, kvp.Value);
+            tableAbdomen.AddRow(kvpa.Key, kvpa.Value);
         }
         tableAbdomen.Write();
         Console.WriteLine();
-        foreach (var kvp in leftLeg)
+
+        var leftLeg = MotorSequenceLeftLeg.ReturnDictionaryOfPositions(Limbic.LeftLeg);
+        var tableLeftLeg = new ConsoleTable("left-leg-motor", "position");
+        foreach (var kvpl in leftLeg)
         {
-            tableLeftLeg.AddRow(kvp.Key, kvp.Value);
+            tableLeftLeg.AddRow(kvpl.Key, kvpl.Value);
         }
         tableLeftLeg.Write();
         Console.WriteLine();
-        foreach (var kvp in rightLeg)
+
+        var rightLeg = MotorSequenceRightLeg.ReturnDictionaryOfPositions(Limbic.RightLeg);
+        var tablerightLeg = new ConsoleTable("right-leg-motor", "position");
+        foreach (var kvpr in rightLeg)
         {
-            tablerightLeg.AddRow(kvp.Key, kvp.Value);
+            tablerightLeg.AddRow(kvpr.Key, kvpr.Value);
         }
         tablerightLeg.Write();
         Console.WriteLine();
-        Console.ReadLine(); // Wait for you to view the position values.
+        Console.WriteLine("If wanting to rescan, type 'do'.");
+        var input = Console.ReadLine(); // Wait for you to view the position values.
+        if (input == "do")
+            goto repeat;
 
         // Dispose resources.
         MotorFunctions.DisposeDynamixelMotors();
