91e7d2be04f21c8136b966dde584c9ebe70b6641
cartheur <cartheur@pm.me>
2025-04-25 16:07:24 +0200

    Organize enum

--- sdk/Limbic.cs
+++ sdk/Limbic.cs
@@ -9,7 +9,7 @@ namespace Cartheur.Animals.Robot
     /// <remarks>Pelvis is a part of the respective leg-limbic areas.</remarks>
     public static class Limbic
     {
-        public enum LimbicArea { Abdomen, All, Bust, Head, LeftArm, RightArm, LeftPelvis, RightPelvis, LeftLeg, RightLeg, Arms, Legs }
+        public enum LimbicArea { Abdomen, All, Arms, Bust, Head, LeftArm, Legs, RightArm, LeftPelvis, RightPelvis, LeftLeg, RightLeg }
 
         public static readonly string[] Abdomen = { "abs_y", "abs_x", "abs_z" };
         public static readonly string[] Bust = { "bust_y", "bust_x" };
@@ -25,7 +25,7 @@ namespace Cartheur.Animals.Robot
         public static readonly string[] RightLegNoPelvis = { "r_hip_z", "r_hip_y", "r_knee_y", "r_ankle_y" };
         public static readonly string[] LeftAnkle = { "l_ankle_y" };
         public static readonly string[] RightAnkle = { "r_ankle_y" };
-        // Regions
+        // Regions.
         public static readonly string[] Arms = { "bust_y", "bust_x", "l_shoulder_y", "l_shoulder_x", "l_arm_z", "l_elbow_y", "r_shoulder_y", "r_shoulder_x", "r_arm_z", "r_elbow_y" };
         public static readonly string[] Legs = { "l_hip_x", "l_hip_z", "l_hip_y", "l_knee_y", "l_ankle_y", "r_hip_x", "r_hip_z", "r_hip_y", "r_knee_y", "r_ankle_y" };
 
